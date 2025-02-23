b834b757c88d88c5932ad56a9496223745679308
Marcos <marcosmartostuvi@gmail.com>
2025-02-23 15:06:14 -0300

    cambios en el navbar

--- frontend/src/layout/navbar/Navbar.jsx
+++ frontend/src/layout/navbar/Navbar.jsx
@@ -20,11 +20,10 @@ const Navbar = () => {
               className="h-8"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-600 dark:text-gray-600">
               Flowbite
             </span>
           </Link>
-
           <div className="flex md:order-2">
             <button
               type="button"
@@ -54,7 +53,7 @@ const Navbar = () => {
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  className="w-4 h-4 text-gray-700 dark:text-gray-600"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -72,21 +71,20 @@ const Navbar = () => {
               <input
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-200 rounded-full bg-gray-200 focus:ring-green-500 focus:border-green-500 dark:bg-gray-s00  dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                 placeholder="Search..."
               />
             </div>
           </div>
-
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-search"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-600">
               <li>
                 <Link
                   to="/"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className="block py-2 px-3 text-gray-700 rounded-sm md:bg-transparent md:text-gray-700 md:p-0 md:dark:text-gray-700"
                 >
                   Home
                 </Link>
@@ -94,7 +92,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/about"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-gray-600 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 md:dark:hover:text-green-500 dark:text-gray-600 dark:hover:bg-gray-200 dark:hover:text-gray-600 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   About
                 </Link>
@@ -102,7 +100,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/services"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-gray-600 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500 md:p-0 dark:text-gray-600 md:dark:hover:text-green-500 dark:hover:bg-gray-200 dark:hover:text-gray-600 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
                 </Link>
